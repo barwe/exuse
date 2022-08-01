24383cc98c7fa773423379fde16642260c111d63
barwe <995488247@qq.com>
2022-08-01 10:48:30 +0800

    init

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name='exuse',
+    version='0.0.1',
+    author='barwe',
+    author_email='barwechin@163.com',
+    url='',
+    description='简单的使用简单的工具',
+    packages=find_packages(),
+    install_requires=[],
+)
