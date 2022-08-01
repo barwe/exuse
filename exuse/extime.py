a622f9898a59154ac988044c91e606d3d95812c5
barwe <995488247@qq.com>
2022-08-01 11:29:36 +0800

    add extime module

---
+++ exuse/extime.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+
+def timestamp(fmt:str=None):
+    """Timestamp for now.
+
+    Args:
+        fmt (str, optional): datetime format. Defaults to `'%Y%m%d_%H%M%S'`.
+
+    Returns:
+        str: formatted datetime.
+    """
+    if fmt is None:
+        fmt = '%Y%m%d_%H%M%S'
+    return datetime.now().strftime(fmt)
